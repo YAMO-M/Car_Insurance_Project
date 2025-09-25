9d799f574116b597b9fc0eb596e114d5ca4dfb7e
Minenhle Mzobe <224057676@stu.ukzn.ac.za>
2025-09-25 22:40:22 +0200

    menuStrip color change

--- WindowsFormsApp1/HomeForm.cs
+++ WindowsFormsApp1/HomeForm.cs
@@ -23,10 +23,6 @@ namespace WindowsFormsApp1
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            homePage2.BringToFront();
-        }
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -43,15 +39,13 @@ namespace WindowsFormsApp1
             
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            addVehicle1.BringToFront();
-        }
 
-        private void button4_Click(object sender, EventArgs e)
-        {
-            makeClaim2.BringToFront();
-        }
+        
+            
+        
+
+
+        
 
 
         private void HomeForm_Load(object sender, EventArgs e)
@@ -59,74 +53,35 @@ namespace WindowsFormsApp1
 
         }
 
-        private void UpdatePersonalDetButton_Click(object sender, EventArgs e)
-        {
-            updatePersonalInfo1.BringToFront();
-        }
 
        
 
       
 
-        private void AddPolicyButton_Click(object sender, EventArgs e)
-        {
-            addVehicle3.BringToFront();
-        }
 
-        private void LogoutButton_Click(object sender, EventArgs e)
-        {
-            string title = "Warning";
-            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
-            string message = "Are you sure you want to Logout?";
-            MessageBoxIcon icon = MessageBoxIcon.Question;
+        
 
-            DialogResult result = MessageBox.Show(message, title, buttons, icon);
 
-            if (result == DialogResult.Yes)
-            {
-                this.Close();
-            }
-        }
 
-        private void button1_Click_1(object sender, EventArgs e)
-        {
-            searchPage1.BringToFront();
-        }
+
 
         private void searchPage1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click_1(object sender, EventArgs e)
-        {
-           DialogResult result = MessageBox.Show("Are you sure you want to Logout?", "Logout", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
-            if (result == DialogResult.Yes)
-            {
                 
-                this.Close();
-            }
-        }
 
         private void homeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             homePage2.BringToFront();
         }
 
-        private void addPolicyToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //addVehicle3.BringToFront();
-        }
+        
 
-        private void makeClaimToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-           // makeClaim2.BringToFront();
-        }
+        
 
-        private void addClientToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            //updatePersonalInfo1.BringToFront();
-        }
+        
 
         private void searchToolStripMenuItem_Click(object sender, EventArgs e)
         {
