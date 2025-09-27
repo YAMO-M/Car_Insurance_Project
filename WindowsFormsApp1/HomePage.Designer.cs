232c3e01307770681b238ed5f3bdd8f566ce8740
Musawenkosi <musakhumalo1205@gmail.com>
2025-09-27 16:36:06 +0200

    reduced the size of the home picture

--- WindowsFormsApp1/HomePage.Designer.cs
+++ WindowsFormsApp1/HomePage.Designer.cs
@@ -126,10 +126,10 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.cars;
-            this.pictureBox1.Location = new System.Drawing.Point(718, 154);
+            this.pictureBox1.Location = new System.Drawing.Point(717, 239);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(987, 769);
+            this.pictureBox1.Size = new System.Drawing.Size(987, 604);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
